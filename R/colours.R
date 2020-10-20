c3e072fe8ebeea5f0021d32902eb1a2389267751
jemunro <jacob.e.munro@gmail.com>
2020-10-20 15:44:02 +1100

    add exports and imports

--- R/colours.R
+++ R/colours.R
@@ -1,20 +1,30 @@
-
+#' @export
 wehi_yellow     <- '#ECE953'
+#' @export
 wehi_green      <- '#49A942'
+#' @export
 wehi_blue       <- '#2372B9'
+#' @export
 wehi_grey       <- '#58595B'
+#' @export
 wehi_orange     <- '#F37520'
+#' @export
 wehi_red        <- '#E01545'
+#' @export
 wehi_purple     <- '#7D489C'
+#' @export
 wehi_light_blue <- '#00BCE7'
+#' @export
 wehi_teal       <- '#00A99F'
 
+#' @export
 wehi_primary <-
   c(wehi_yellow = wehi_yellow,
     wehi_green = wehi_green,
     wehi_blue = wehi_blue,
     wehi_grey = wehi_grey)
 
+#' @export
 wehi_secondary <-
   c(wehi_orange = wehi_orange,
     wehi_red = wehi_red,
@@ -22,16 +32,19 @@ wehi_secondary <-
     wehi_light_blue = wehi_light_blue,
     wehi_teal = wehi_teal)
 
+#' @export
 wehi_extended <-
   c(wehi_yellow = wehi_yellow,
-    wehi_green = wehi_green,
     wehi_blue = wehi_blue,
+    wehi_green = wehi_green,
     wehi_orange = wehi_orange,
-    wehi_red = wehi_red,
     wehi_purple = wehi_purple,
+    wehi_red = wehi_red,
     wehi_light_blue = wehi_light_blue,
-    wehi_teal = wehi_teal)
+    wehi_teal = wehi_teal,
+    wehi_grey = wehi_grey)
 
+#' @export
 wehi_ordered <-
   c(wehi_red = wehi_red,
     wehi_orange = wehi_orange,
--- R/scales.R
+++ R/scales.R
@@ -25,16 +25,69 @@ wehi_ordered_pal <- function(n) {
   return(pal)
 }
 
-wehi_cividis_pal <- function(n) {
-  stopifnot(rlang::is_scalar_integerish(n) && n > 0)
-  colorRampPalette(c(wehi_blue, wehi_grey, wehi_yellow))(n)
+wehi_continous <- function(direction, n=1000) {
+  stopifnot(rlang::is_scalar_integerish(direction))
+  if (direction == -1) {
+    pal <- colorRampPalette(c(wehi_yellow, wehi_grey, wehi_blue))(n)
+  } else {
+    pal <- colorRampPalette(c(wehi_blue, wehi_grey, wehi_yellow))(n)
+
+  }
+  function(x) { ramp2(pal, x) }
 }
 
-scale_fill_wehi <- function (..., type = "seq", aesthetics = "fill") {
+wehi_continous2 <- function(direction, n=1000) {
+  stopifnot(rlang::is_scalar_integerish(direction))
+  if (direction == -1) {
+    pal <- colorRampPalette(c(wehi_yellow, wehi_green, wehi_blue))(n)
+  } else {
+    pal <- colorRampPalette(c(wehi_blue, wehi_green, wehi_yellow))(n)
+  }
+  function(x) { ramp2(pal, x) }
+}
+
+
+ramp2 <- function(pal, x) {
+  i <-  as.integer(round(x * length(pal)))
+  pal[i]
+}
+
+#' @export
+scale_fill_wehi <- function (..., aesthetics = "fill") {
   ggplot2::discrete_scale(aesthetics, "wehi", wehi_pal, ...)
 }
 
-scale_fill_wehi_ordered <- function (..., type = "seq", aesthetics = "fill") {
+#' @export
+scale_fill_wehi_ordered <- function (..., aesthetics = "fill") {
   ggplot2::discrete_scale(aesthetics, "wehi_ordered", wehi_ordered_pal, ...)
 }
+
+#' @export
+scale_fill_wehi_continuous <- function (..., aesthetics = "fill", direction = 1,
+                                        na.value = 'grey50', guide = 'colourbar') {
+  ggplot2::continuous_scale(aesthetics, "wehi_continuous", wehi_continous(direction),
+                            na.value = na.value, guide = guide, ...)
+}
+
+#' @export
+scale_fill_wehi_continuous2 <- function (..., aesthetics = "fill", direction = 1,
+                                         na.value = 'grey50', guide = 'colourbar') {
+  ggplot2::continuous_scale(aesthetics, "wehi_continuous2", wehi_continous2(direction),
+                            na.value = na.value, guide = guide, ...)
+}
+
+#' @export
+scale_colour_wehi_continuous <- function (..., aesthetics = "colour", direction = 1,
+                                          na.value = 'grey50', guide = 'colourbar') {
+  ggplot2::continuous_scale(aesthetics, "wehi_continuous", wehi_continous(direction),
+                            na.value = na.value, guide = guide, ...)
+}
+
+#' @export
+scale_colour_wehi_continuous2 <- function (..., aesthetics = "colour", direction = 1,
+                                           na.value = 'grey50', guide = 'colourbar') {
+  ggplot2::continuous_scale(aesthetics, "wehi_continuous2", wehi_continous2(direction),
+                            na.value = na.value, guide = guide, ...)
+}
+
 
