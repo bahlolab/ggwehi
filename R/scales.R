a6c547c31fca63e6ce249912dd141297fcec297b
jemunro <jacob.e.munro@gmail.com>
2020-10-23 16:20:12 +1100

    update

--- R/scales.R
+++ R/scales.R
@@ -47,9 +47,19 @@ scale_color_wehi_paired  <- function (..., aesthetics = "colour", na.value = weh
 
 #' @export
 scale_fill_wehi_continuous <- function (..., aesthetics = "fill", direction = 1,
-                                        na.value = wehi_grey, guide = 'colourbar') {
-  ggplot2::continuous_scale(aesthetics, "wehi_continuous", wehi_continous(direction),
-                            na.value = na.value, guide = guide, ...)
+                                        na.value = wehi_grey, guide = 'colourbar',
+                                        option = 'A') {
+  if (!(rlang::is_scalar_character(option) && option %in% c('A', 'B'))) {
+    rlang::warn('"option" must be one of c("A", "B")')
+    option <- 'A'
+  }
+  if (option == 'A') {
+    ggplot2::continuous_scale(aesthetics, "wehi_continuous_a", wehi_continous_a(direction),
+                              na.value = na.value, guide = guide, ...)
+  } else {
+    ggplot2::continuous_scale(aesthetics, "wehi_continuous_b", wehi_continous_b(direction),
+                              na.value = na.value, guide = guide, ...)
+  }
 }
 
 #' @export
@@ -73,7 +83,7 @@ scale_colour_wehi_continuous <- function (..., aesthetics = "colour", direction 
 scale_color_wehi_continuous <- function (..., aesthetics = "colour", direction = 1,
                                          na.value = wehi_grey, guide = 'colourbar',
                                          option = 'A') {
-  if (!(rlang::is_scalar_character(option) && !option %in% c('A', 'B'))) {
+  if (!(rlang::is_scalar_character(option) && option %in% c('A', 'B'))) {
     rlang::warn('"option" must be one of c("A", "B")')
     option <- 'A'
   }
